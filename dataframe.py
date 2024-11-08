7e645143b1a59e70c3a401298fe110cb19dada02
florentinaibarbia <florentinaibarbia@gmail.com>
2024-11-08 11:13:22 -0300

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 data = {
-    'date': ['blue','green','yellow','red','white'],
+    'date': ['pink','green','yellow','red','white'],
     'city': ['London', 'Tokyo', 'Dubai', 'Sydney', 'New York'],
     'price': [1.2, 1.0, 0.6, 0.9, 1.7]
 }
