172b1d7b8a2508d837e2da887e14cdfaa472e555
Paulita11111 <pcostame26@ucema.edu.ar>
2024-11-08 11:42:28 -0300

    Add files via upload

---
+++ api de terceros.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+def obtener_datos_meteorologicos(ciudad):
+    # Tu API key de OpenWeather
+    api_key = 'tu_api_key_aqui'
+    
+    # URL de la API (usamos el endpoint de clima actual)
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric'
+    
+    # Hacer la solicitud GET
+    response = requests.get(url)
+    
+    if response.status_code == 200:  # Si la solicitud fue exitosa
+        # Parsear los datos JSON
+        data = response.json()
+
+        # Extraer los datos que necesitas
+        temp_maxima = data['main']['temp_max']
+        temp_minima = data['main']['temp_min']
+        fecha = data['dt']  # Timestamp en segundos (puedes convertirlo a una fecha legible)
+
+        return fecha, ciudad, temp_maxima, temp_minima
+    else:
+        print("Error al obtener los datos de la API")
+        return None
